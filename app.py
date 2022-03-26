2496c16c12430ec41db11ae71f7479904dff54cf
rusoyc <54208027+rusoyc@users.noreply.github.com>
2022-03-26 16:40:57 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+import joblib
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        purchases = request.form.get("purchases")
+        suppcard = request.form.get("suppcard")
+        print(purchases,suppcard)
+        
+        purchases = float(purchases)
+        suppcard = float(suppcard)
+        
+        model1 = joblib.load("CART")
+        pred1 = model1.predict([[purchases,suppcard]])
+
+        model2 = joblib.load("RF")
+        pred2 = model2.predict([[purchases,suppcard]])
+        
+        model3 = joblib.load("GB")
+        pred3 = model3.predict([[purchases,suppcard]])
+        return(render_template("index.html",result1=pred1,result2=pred2,result3=pred3))
+    else:
+        return(render_template("index.html",result1="",result2="",result3=""))
+
+if __name__ == "__main__":
+    app.run()
